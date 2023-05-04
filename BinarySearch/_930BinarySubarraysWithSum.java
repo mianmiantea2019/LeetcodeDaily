560612c438dfade7fce69c71d36bdbac562d338d
mianmiantea2019 <59427068+mianmiantea2019@users.noreply.github.com>
2023-05-03 23:47:38 -0400

    Create _930BinarySubarraysWithSum.java

---
+++ BinarySearch/_930BinarySubarraysWithSum.java
@@ -0,0 +1,25 @@
+package BinarySearch;
+
+/**
+ * @Author: mianmiantea2019
+ * @Date: 2023-05-03 10:57 PM
+ * @LastEditors: mianmiantea2019
+ * @Description:
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class _930BinarySubarraysWithSum {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0, sum = 0;
+        for (int num : nums) {
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+            System.out.println(map);
+            sum += num;
+            count += map.getOrDefault(sum - goal, 0);
+        }
+        return count;
+    }
+}
