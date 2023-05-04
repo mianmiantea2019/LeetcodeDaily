ce8ad9c8ff2a849ec695bab4e2ddcf129936471f
mianmiantea2019 <59427068+mianmiantea2019@users.noreply.github.com>
2023-05-04 00:00:54 -0400

    Create _759EmployeeFreeTime.java

---
+++ ScanLine/_759EmployeeFreeTime.java
@@ -0,0 +1,48 @@
+package scanLine;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+/**
+ * @Author: mianmiantea2019
+ * @Date: 2023-05-03 7:15 PM
+ * @LastEditors: mianmiantea2019
+ * @Description:
+ */
+
+public class _759_EmployeeFreeTime {
+    class Interval {
+        public int start;
+        public int end;
+
+        public Interval() {}
+
+        public Interval(int _start, int _end) {
+            start = _start;
+            end = _end;
+        }
+    };
+    public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
+        List<Interval> res = new ArrayList<>();
+
+        PriorityQueue<Interval> pq = new PriorityQueue<>((a, b) -> {
+            return a.start - b.start;
+        });
+        for(List<Interval> i: schedule){
+            for(Interval j: i){
+                pq.add(j);
+            }
+        }
+
+        Interval last = pq.poll();
+        while(!pq.isEmpty()){
+            Interval cur = pq.poll();
+            if(last.end < cur.start){
+                res.add(new Interval(last.end, cur.start));
+            }
+            last.end = Math.max(last.end, cur.end);
+        }
+        return res;
+    }
+}
