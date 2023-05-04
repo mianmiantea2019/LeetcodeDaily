5f292058e0c559b7e828c15910c2461356c22e3a
mianmiantea2019 <59427068+mianmiantea2019@users.noreply.github.com>
2023-05-04 00:00:27 -0400

    Create _435NonoverlappingIntervals.java

---
+++ ScanLine/_435NonoverlappingIntervals.java
@@ -0,0 +1,32 @@
+package scanLine;
+
+import java.util.Arrays;
+
+/**
+ * @Author: mianmiantea2019
+ * @Date: 2023-05-03 7:15 PM
+ * @LastEditors: mianmiantea2019
+ * @Description:
+ */
+
+public class _435_Non_overlappingIntervals {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        if(intervals.length == 0) return 0;
+
+        Arrays.sort(intervals,(a1, a2)->{
+            return a1[1] - a2[1];
+        });
+
+        int end = Integer.MIN_VALUE;
+        int count = 0;
+        for(int[] cur: intervals){
+            if(cur[0] >= end){
+                end = cur[1];
+            }else{
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
