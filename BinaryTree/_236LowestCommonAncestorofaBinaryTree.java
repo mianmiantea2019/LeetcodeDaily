a3a1713a4086218c22f2b414b7f78b1f509b66f9
mianmiantea2019 <59427068+mianmiantea2019@users.noreply.github.com>
2023-05-15 19:02:03 -0400

    Create _236LowestCommonAncestorofaBinaryTree.java

---
+++ BinaryTree/_236LowestCommonAncestorofaBinaryTree.java
@@ -0,0 +1,26 @@
+package BinaryTree;
+
+/**
+ * @Author: mianmiantea2019
+ * @Date: 2023-05-15
+ * @LastEditors: mianmiantea2019
+ * @Description:
+ */
+public class _236LowestCommonAncestorofaBinaryTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        } else if (left != null) {
+            return left;
+        } else {
+            return right;
+        }
+    }
+}
